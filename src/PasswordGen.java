44bf5f6d7921e5d1525c10e24dade5899eb61adf
mbroer <mbroer95@gmail.com>
2021-05-04 23:29:18 +0200

    Created passwordgen functionality

--- src/PasswordGen.java
+++ src/PasswordGen.java
@@ -18,17 +18,32 @@ public class PasswordGen
     //generates a random string with specified length
     public static String generate(int length)
     {
-        return "";
+        StringBuilder str = new StringBuilder();
+        str.append(CHARS).append(LCCHARS).append(NUMS);
+
+        if(getIncludeSpecialChars())
+            str.append(SPECIAL);
+
+        StringBuilder shuffled = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            int index = random.nextInt(str.length());
+            shuffled.append(str.charAt(index));
+        }
+
+        return shuffled.substring(0, length).toString();
     }
 
     //allows user to toggle special characters on and off for password generator
     public static void setIncludeSpecialChars(boolean bool)
     {
+        includeSpecialChars = bool;
     }
 
     //returns if passwordgen should use special characters in generate()
     public static boolean getIncludeSpecialChars()
     {
-        return true;
+        return includeSpecialChars;
     }
 }
